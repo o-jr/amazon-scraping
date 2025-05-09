24c255a41e7eec226ed65f27ca73401353a4110a
OJ <orlandojunior.py@gmail.com>
2025-05-08 21:44:43 -0300

    start parsing amazon

--- extract/extract/settings.py
+++ extract/extract/settings.py
@@ -12,6 +12,10 @@ BOT_NAME = "extract"
 SPIDER_MODULES = ["extract.spiders"]
 NEWSPIDER_MODULE = "extract.spiders"
 
+USER_AGENT = 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
+DOWNLOAD_DELAY = 2 
+RANDOMIZE_DOWNLOAD_DELAY = True
+
 ADDONS = {}
 
 
